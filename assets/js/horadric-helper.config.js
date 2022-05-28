f1fa1eaff2e02576753f9606bcf83dbf445d9766
Darin Randal <darin.randal@gmail.com>
2022-05-27 22:32:41 -0400

    horadric helper config setup and test

--- assets/js/horadric-helper.config.js
+++ assets/js/horadric-helper.config.js
@@ -1,8 +1,39 @@
 window.HoradricHelper.PathOfExile.applyConfig([
   {
-    "reference": "Albino Rhoa Feather",
+    "reference": "albino-rhoa-feather",
       "iconUrl": "/assets/img/horadric-helper/CurrencyRhoaFeather.png",
-      "data": "Item Class: Stackable Currency\nRarity: Currency\nAlbino Rhoa Feather\n--------\nStack Size: 1/10\n--------\nShift click to unstack."
+      "data": `Item Class: Stackable Currency
+      Rarity: Currency
+      Albino Rhoa Feather
+      --------
+      Stack Size: 1/10`
+  },
+  {
+    "reference": "areas-are-alluring",
+      "iconUrl": "/assets/img/horadric-helper/ChargedCompass.png",
+      "data": `Item Class: Stackable Currency
+      Rarity: Normal
+      Charged Compass
+      --------
+      Item Level: 60
+      --------
+      Your Maps are Alluring (enchant)
+      4 uses remaining (enchant)
+      --------
+      Right click on this item then left click on a Voidstone to apply the itemised Sextant Modifier to the Voidstone.`
+  },
+  {
+    "reference": "gilded-fossil",
+      "iconUrl": "/assets/img/horadric-helper/GildedFossil.png",
+      "data": `Item Class: Stackable Currency
+      Rarity: Currency
+      Gilded Fossil
+      --------
+      Stack Size: 1/10
+      --------
+      Item is overvalued by vendors
+      --------
+      Place in a Resonator to influence item crafting.`
   },
   {
     reference: "pitfighter",
@@ -204,5 +235,506 @@ window.HoradricHelper.PathOfExile.applyConfig([
     "They say that you should take life's troubles in your stride.
     Some men have a longer stride than others."
     - Gaius Sentari`
+  },
+  {
+    reference: "writhing-jar",
+    iconUrl: "/assets/img/horadric-helper/WrithingJar.png",
+    data: `Item Class: Hybrid Flasks
+    Rarity: Unique
+    The Writhing Jar
+    Hallowed Hybrid Flask
+    --------
+    Recovers 452 (augmented) Life over 5 Seconds
+    Recovers 125 (augmented) Mana over 5 Seconds
+    Consumes 17 (augmented) of 40 Charges on use
+    Currently has 0 Charges
+    --------
+    Requirements:
+    Level: 60
+    --------
+    Item Level: 84
+    --------
+    15% reduced Charges per use
+    74% reduced Amount Recovered
+    Instant Recovery
+    2 Enemy Writhing Worms escape the Flask when used
+    Writhing Worms are destroyed when Hit
+    --------
+    To achieve godhood, they would cast off all that god has gifted us.
+    Mortality, compassion, even the sanctity of flesh.
+     - High Templar Voll
+    --------
+    Right click to drink. Can only hold charges while in belt. Refills as you kill monsters.`
+  },
+  {
+    reference: "rot-head-talisman",
+    iconUrl: "/assets/img/horadric-helper/RotHeadTalisman.png",
+    data: `Item Class: Amulets
+    Rarity: Rare
+    Soul Noose
+    Rot Head Talisman
+    --------
+    Requirements:
+    Level: 56
+    --------
+    Item Level: 83
+    --------
+    Talisman Tier: 1
+    --------
+    Allocates Blade Barrier (enchant)
+    --------
+    34% increased Fish Bite Sensitivity (implicit)
+    --------
+    +35 to Strength
+    +21 to Dexterity
+    12% increased maximum Energy Shield
+    0.66% of Physical Attack Damage Leeched as Mana
+    +14 Life gained on Kill
+    --------
+    To catch a big fish you need tempting bait. 
+    And there is no fish bigger than the Empire,
+    and no bait as tempting as Ezomyr.
+    - The Wolven King
+    --------
+    Corrupted`
+  },
+  {
+    reference: "anglers-plait",
+    iconUrl: "/assets/img/horadric-helper/AnglersPlait.png",
+    data: `Item Class: Rings
+    Rarity: Unique
+    Angler's Plait
+    Unset Ring
+    --------
+    Requirements:
+    Level: 38
+    --------
+    Item Level: 85
+    --------
+    You can catch Corrupted Fish (implicit)
+    --------
+    12% increased Cast Speed
+    100% increased Fishing Line Strength
+    20% increased Quantity of Fish Caught
+    40% increased Rarity of Fish Caught
+    27% increased Fish Bite Sensitivity
+    --------
+    A man can spend his whole life fishing
+    before he learns it is not fish that he seeks.
+    --------
+    Corrupted`
+  },
+  {
+    reference: "replica-bated-breath",
+    iconUrl: "/assets/img/horadric-helper/BatedBreath.png",
+    data: `Item Class: Belts
+    Rarity: Unique
+    Replica Bated Breath
+    Chain Belt
+    --------
+    Requirements:
+    Level: 22
+    --------
+    Item Level: 80
+    --------
+    +15 to maximum Energy Shield (implicit)
+    --------
+    -23 to Intelligence
+    10% increased Damage
+    50% increased Fishing Pool Consumption
+    20% increased Fishing Range
+    26% increased Rarity of Fish Caught
+    --------
+    "I cannot fathom the mysteries of Prototype #16. It did nearly nothing until I
+    brought it to [RESTRICTED]. It makes no sense!"
+    - Researcher Graven`
+  },
+  {
+    reference: "anomalous-portal",
+    iconUrl: "/assets/img/horadric-helper/Portal.png",
+    data: `Item Class: Active Skill Gems
+    Rarity: Gem
+    Anomalous Portal
+    --------
+    Spell
+    Level: 1 (Max)
+    Cast Time: 2.50 sec
+    Quality: +20% (augmented)
+    Alternate Quality
+    --------
+    Requirements:
+    Level: 10
+    --------
+    Creates a portal to the current area's town.
+    --------
+    0.20% chance to travel to Winding Pier instead while on a Ley Fishing Line
+    --------
+    Place into an item socket of the right colour to gain this skill. Right click to remove from a socket.`
+  },
+  {
+    reference: "the-ancient-angler",
+    iconUrl: "/assets/img/horadric-helper/TheAncientAngler.png",
+    data: `Item Class: Sentinel
+    Rarity: Unique
+    The Ancient Angler
+    Emberstone Apex Sentinel
+    --------
+    Duration: 30 seconds
+    Empowers: 2 enemies
+    Empowerment: 45
+    --------
+    Requirements:
+    Level: 58
+    --------
+    Item Level: 85
+    --------
+    Can only empower Rare or Unique enemies (implicit)
+    --------
+    Cleanses Corrupted Fish
+    Purifies Corrupted Water
+    --------
+    Charge: 10/10
+    --------
+    Under clouds of black beset by crimson lightning, we sailed stormy seas of poison and disease. Only one light shone in that darkness; only one Flame to keep hope alive.
+    --------
+    Unmodifiable`
+  },
+  {
+    reference: "the-infested",
+    iconUrl: "/assets/img/horadric-helper/TheInfested.png",
+    data: `Item Class: Sentinel
+    Rarity: Unique
+    The Infested
+    Obsidian Stalker Sentinel
+    --------
+    Duration: 50 seconds
+    Empowers: 85 enemies
+    Empowerment: 34
+    --------
+    Requirements:
+    Level: 67
+    --------
+    Item Level: 72
+    --------
+    Spawns Worms
+    --------
+    Charge: 6/8
+    --------
+    Mechanisms gone awry, circuitry in flux.
+    This experiment became something truly perverse.
+    --------
+    Unmodifiable`
+  },
+  {
+    reference: "star-of-wraeclast",
+    iconUrl: "/assets/img/horadric-helper/StarOfWraeclast.png",
+    data: `Item Class: Amulets
+    Rarity: Unique
+    Star of Wraeclast
+    Ruby Amulet
+    --------
+    Requirements:
+    Level: 28
+    --------
+    Item Level: 85
+    --------
+    +25% to Fire Resistance (implicit)
+    --------
+    Grants Level 10 Frostblink Skill
+    39% increased Cold Damage
+    +14% to all Elemental Resistances
+    60% increased Area of Effect of Hex Skills
+    You cannot be Cursed with Silence
+    Frostblink has 50% increased Duration
+    --------
+    "I offer to you an eternal oath 
+    that binds your heart to mine;
+    a bond that not even death will break.
+    Will you accept?"
+    - Daresso, to his beloved
+    --------
+    Corrupted`
+  },
+  {
+    reference: "yriels-fostering",
+    iconUrl: "/assets/img/horadric-helper/Yriels-Fostering.png",
+    data: `Item Class: Body Armours
+    Rarity: Unique
+    Yriel's Fostering
+    Exquisite Leather
+    --------
+    Evasion Rating: 1798 (augmented)
+    --------
+    Requirements:
+    Level: 62
+    Dex: 170
+    --------
+    Sockets: B G 
+    --------
+    Item Level: 72
+    --------
+    Grants Level 20 Summon Bestial Snake Skill
+    +390 to Accuracy Rating
+    146% increased Evasion Rating
+    +93 to maximum Life
+    Projectile Attack Skills have 54% increased Critical Strike Chance
+    Projectiles from Attacks have 20% chance to Poison on Hit while
+    you have a Bestial Minion
+    Adds 18-28 Chaos Damage to Attacks while you have a Bestial Minion
+    --------
+    Feed a beast and it will not hunt.
+    Protect it and it will not fight.
+    Ferocity must be learned, not taught.
+    It is suffering that forges the greatest warriors.`
+  },
+  {
+    reference: "the-goddess-bound",
+    iconUrl: "/assets/img/horadric-helper/TheGoddessBound.png",
+    data: `Item Class: Thrusting One Hand Swords
+    Rarity: Unique
+    The Goddess Bound
+    Whalebone Rapier
+    --------
+    One Handed Sword
+    Physical Damage: 16-66 (augmented)
+    Elemental Damage: 3-7 (augmented)
+    Critical Strike Chance: 5.50%
+    Attacks per Second: 1.92 (augmented)
+    Weapon Range: 14
+    --------
+    Requirements:
+    Level: 7
+    Dex: 32
+    --------
+    Sockets: G-B G 
+    --------
+    Item Level: 76
+    --------
+    +25% to Global Critical Strike Multiplier (implicit)
+    --------
+    +1 to Level of Socketed Melee Gems
+    Uses both hand slots
+    290% increased Physical Damage
+    Adds 3 to 7 Fire Damage
+    20% increased Attack Speed
+    30% increased Global Critical Strike Chance
+    +33 to Evasion Rating
+    20% reduced Rarity of Items found
+    30% increased Movement Speed when on Low Life
+    --------
+    She paints her offer in wicked hues
+    An off-white grin, an elegant bruise.
+    To the nascent scourge she sings the ruse:
+    "With me in hand, what else need you use?"`
+  },
+  {
+    reference: "allure",
+    iconUrl: "/assets/img/horadric-helper/Allure.png",
+    data: `Item Class: Claws
+    Rarity: Unique
+    Allure
+    Vaal Claw
+    --------
+    Claw
+    Physical Damage: 101-231 (augmented)
+    Critical Strike Chance: 6.50%
+    Attacks per Second: 1.40 (augmented)
+    Weapon Range: 11
+    --------
+    Requirements:
+    Level: 66
+    Dex: 95
+    Int: 131
+    --------
+    Sockets: G 
+    --------
+    Item Level: 76
+    --------
+    2% of Physical Attack Damage Leeched as Life (implicit)
+    --------
+    110% increased Physical Damage
+    Adds 19 to 34 Physical Damage
+    8% increased Attack Speed
+    15% increased Movement Speed while Phasing
+    You gain Phasing for 10 seconds on using a Vaal Skill
+    --------
+    What drives us to kill, to carve and to shred?
+    What drives us to drink the seething souls of the dead?
+    What drives us to love and to hold, but still go amiss?
+    What drives us to leave this world for the Abyss?`
+  },
+  {
+    reference: "bated-breath",
+    iconUrl: "/assets/img/horadric-helper/BatedBreath.png",
+    data: `Item Class: Belts
+    Rarity: Unique
+    Bated Breath
+    Chain Belt
+    --------
+    Requirements:
+    Level: 22
+    --------
+    Item Level: 85
+    --------
+    +15 to maximum Energy Shield (implicit)
+    --------
+    +17 to Intelligence
+    10% increased Damage
+    +29 to maximum Energy Shield
+    20% increased maximum Energy Shield
+    50% increased Energy Shield Recharge Rate
+    --------
+    At knifepoint, a moment's hesitation means death.`
+  },
+  {
+    reference: "wurms-molt",
+    iconUrl: "/assets/img/horadric-helper/WurmsMolt.png",
+    data: `Item Class: Belts
+    Rarity: Unique
+    Wurm's Molt
+    Leather Belt
+    --------
+    Requirements:
+    Level: 8
+    --------
+    Item Level: 77
+    --------
+    +35 to maximum Life (implicit)
+    --------
+    +28 to Strength
+    +29 to Intelligence
+    +10% to Cold Resistance
+    0.4% of Physical Attack Damage Leeched as Life
+    0.4% of Physical Attack Damage Leeched as Mana
+    --------
+    They say a lifetime of wisdom is carried
+    in every skin the great beasts shed.`
+  },
+  {
+    reference: "blood-of-corruption",
+    iconUrl: "/assets/img/horadric-helper/HeartOfCorruption.png",
+    data: `Item Class: Amulets
+    Rarity: Unique
+    Blood of Corruption
+    Amber Amulet
+    --------
+    Requirements:
+    Level: 20
+    --------
+    Item Level: 80
+    --------
+    +21 to Strength (implicit)
+    --------
+    Grants Level 10 Gluttony of Elements Skill
+    Adds 19 to 43 Chaos Damage to Attacks
+    -9% to all Elemental Resistances
+    +19% to Chaos Resistance
+    --------
+    In my dream, a voice spoke to me. It said:
+    'My reach knows no bounds.
+    All that is pure is destined to rot.
+    All that lives is destined to serve.'
+    - Doryani, Queen's Thaumaturgist
+    --------
+    Corrupted`
+  },
+  {
+    reference: "blightwell",
+    iconUrl: "/assets/img/horadric-helper/Blightwell.png",
+    data: `Item Class: Amulets
+    Rarity: Unique
+    Blightwell
+    Clutching Talisman
+    --------
+    Requirements:
+    Level: 28
+    --------
+    Item Level: 56
+    --------
+    Talisman Tier: 2
+    --------
+    21% increased Global Defences (implicit)
+    --------
+    +24 to maximum Energy Shield
+    +30% to Fire Resistance
+    +22% to Lightning Resistance
+    50% slower start of Energy Shield Recharge during any Flask Effect
+    174% increased Energy Shield Recharge Rate during any Flask Effect
+    --------
+    Long ago, the ocean was a puddle where a golden fish dwelt,
+    who lit the ocean from within and kept the water fresh.
+    When the sky burned, the fish dived deep,
+    never to return, and the waters turned sour.
+    --------
+    Corrupted`
+  },
+  {
+    reference: "brinerot-whalers",
+    iconUrl: "/assets/img/horadric-helper/BrinerotWhalers.png",
+    data: `Item Class: Boots
+    Rarity: Unique
+    Brinerot Whalers
+    Trapper Boots
+    --------
+    Evasion Rating: 81
+    Energy Shield: 112 (augmented)
+    --------
+    Requirements:
+    Level: 41
+    Dex: 40
+    Int: 40
+    --------
+    Sockets: B-G 
+    --------
+    Item Level: 72
+    --------
+    20% increased Global Physical Damage
+    +95 to maximum Energy Shield
+    +28% to Lightning Resistance
+    25% increased Movement Speed
+    24% increased Projectile Damage
+    Unaffected by Shocked Ground
+    --------
+    Those Theopolis fatcats put a price on our heads.
+    Let's see what they'll pay for their own.`
+  },
+  {
+    reference: "fishscale-gauntlets",
+    iconUrl: "/assets/img/horadric-helper/GlovesStrDex1.png",
+    data: `Item Class: Gloves
+    Rarity: Magic
+    Frosted Fishscale Gauntlets of Skill
+    --------
+    Armour: 9
+    Energy Rating: 9
+    --------
+    Requirements:
+    Level: 2
+    Str: 4
+    Dex: 4
+    --------
+    Item Level: 5
+    --------
+    Adds 1 to 2 Cold Damage to Attacks
+    6% Increased Attack Speed`
+  },
+  {
+    reference: "scale-vest",
+    iconUrl: "/assets/img/horadric-helper/BodyStrDex1A.png",
+    data: `Item Class: Body Armours
+    Rarity: Magic
+    Scale Vest of the Wrestler
+    --------
+    Armour: 29
+    Evasion Rating: 29
+    --------
+    Requirements:
+    Level: 8
+    --------
+    Sockets: R-R-R-R 
+    --------
+    Item Level: 37
+    --------
+    +17 to Strength`
   }
 ]);
---
+++ horadric-helper-test.md
@@ -0,0 +1,63 @@
+---
+layout: page
+title: Horadric Helper test
+permalink: /horadric-helper-test/
+---
+
+<style>
+    .card-container {
+        display: flex;
+        align-items: flex-start;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+        //flex-direction: column;
+        height: 100%;
+        padding: 0;
+        margin: 0;
+    }
+
+    .card-container .card {
+        text-align: center;
+        border: 1px #000 solid;
+        width: 25%;
+        height: 250px;
+        align-items: center;
+        justify-content: center;
+        line-height: 250px;
+    }
+</style>
+
+<poe-item as-showcase icon-inside icon-size="md" reference="albino-rhoa-feather"></poe-item>
+
+<div class="card-container">
+    <div class="card"><poe-passive as-icon reference="pitfighter"></poe-passive></div>
+    <div class="card"><poe-item as-icon reference="child-of-violence"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="albino-rhoa-feather"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="fishing-rod"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="reefbane"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="song-of-the-sirens"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="fairgraves-tricorne"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="slitherpinch"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="rainbowstride"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="writhing-jar"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="areas-are-alluring"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="gilded-fossil"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="rot-head-talisman"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="anglers-plait"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="replica-bated-breath"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="anomalous-portal"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="the-ancient-angler"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="the-infested"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="star-of-wraeclast"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="yriels-fostering"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="the-goddess-bound"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="allure"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="bated-breath"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="wurms-molt"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="blood-of-corruption"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="blightwell"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="brinerot-whalers"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="fishscale-gauntlets"></poe-item></div>
+    <div class="card"><poe-item as-icon reference="scale-vest"></poe-item></div>
+</div>
